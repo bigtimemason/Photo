9a89962624c6991d510d66214248cb33e80e57a5
Mason Miller <44513924+bigtimemason@users.noreply.github.com>
2019-05-11 16:18:27 -0400

    Update wow.js

--- wow.js
+++ wow.js
@@ -2,59 +2,19 @@
 
 
 
+function saveEdits() {
 
+//get the editable element
+var editElem = document.getElementById("edit");
 
+//get the edited element content
+var userVersion = editElem.innerHTML;
 
-var editBtn = document.getElementById('editBtn');
-var editables = document.querySelectorAll('#title, #author, #content')
- 
-editBtn.addEventListener('click', function(e) {
-  if (!editables[0].isContentEditable) {
-    editables[0].contentEditable = 'true';
-    editables[1].contentEditable = 'true';
-    editables[2].contentEditable = 'true';
-    editBtn.innerHTML = 'Save Changes';
-    editBtn.style.backgroundColor = '#6F9';
-  } else {
-    // Disable Editing
-    editables[0].contentEditable = 'false';
-    editables[1].contentEditable = 'false';
-    editables[2].contentEditable = 'false';
-    // Change Button Text and Color
-    editBtn.innerHTML = 'Enable Editing';
-    editBtn.style.backgroundColor = '#F96';
-    // Save the data in localStorage 
-    for (var i = 0; i < editables.length; i++) {
-      localStorage.setItem(editables[i].getAttribute('id'), editables[i].innerHTML);
-    }
-  }
-});
+//save the content to local storage
+localStorage.userEdits = userVersion;
 
-if (typeof(Storage) !== "undefined") {
- 
-  if (localStorage.getItem('title') !== null) {
-    editables[0].innerHTML = localStorage.getItem('title');
-  }
-   
-  if (localStorage.getItem('author') !== null) {
-    editables[1].innerHTML = localStorage.getItem('author');
-  }
-   
-  if (localStorage.getItem('content') !== null) {
-    editables[2].innerHTML = localStorage.getItem('content');
-  } 
+//write a confirmation to the user
+document.getElementById("update").innerHTML="Edits saved!";
+
 }
-
-document.addEventListener('keydown', function(e) {
-  for (var i = 0; i < editables.length; i++) {
-    localStorage.setItem(editables[i].getAttribute('id'), editables[i].innerHTML);
-  }
-});
-
-
-
-
-
-
-
 
