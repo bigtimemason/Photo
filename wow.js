4b2a8f2d2f33daf452e817e45630927d1e2f1fac
Mason Miller <44513924+bigtimemason@users.noreply.github.com>
2019-05-10 21:27:19 -0400

    Update wow.js

--- wow.js
+++ wow.js
@@ -1,3 +1,31 @@
-document.getElementById("editor").addEventListener("input", function() {
-    document.getElementById("log").value += "input event fired\n";
-}, false);
+Here's a way you can do this using vanilla JS.
+
+Here's JSFiddle since StackOverflow doesn't allow localStorage to be executed on the website as a security feature.
+
+How it works:
+
+Using window.onload = function() {...}, check if 'content' key exists in localStorage
+If it does, load the data into <div class="content">
+Upon pressing Edit button, contentEditable is toggled
+Here you can use any method to save the data. I used content.contentEditable === 'false' to save the innerHTML data to 'content' key.
+To Note: localStorage is saved in your browser locally, use databases or anything similar to display edits to all viewers.
+
+// Load content onload if it exists in localStorage
+window.onload = function() {
+	if(localStorage.getItem('content')) {
+		document.querySelector('.content').innerHTML = localStorage.getItem('content');
+  }
+}
+
+let editBtn = document.querySelector('#edit_content');
+let content = document.querySelector('.content');
+
+editBtn.addEventListener('click', () => {
+  // Toggle contentEditable on button click
+	content.contentEditable = !content.isContentEditable;
+  
+  // If disabled, save text
+  if(content.contentEditable === 'false') {
+  	localStorage.setItem('content', content.innerHTML);
+  }
+});
