1d2dd7689c51e1ac865ced36f8b973df4a4af44d
Mason Miller <44513924+bigtimemason@users.noreply.github.com>
2019-05-11 16:02:50 -0400

    Update wow.js

--- wow.js
+++ wow.js
@@ -33,3 +33,62 @@ document.addEventListener('keydown', function (event) {
 function log(s) {
   document.getElementById('debug').innerHTML = 'value changed to: ' + s;
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+document.addEventListener('keydown', function (event) {
+  var esc = event.which == 27,
+      nl = event.which == 13,
+      el = event.target,
+      input = el.nodeName != 'INPUT' && el.nodeName != 'TEXTAREA',
+      data = {};
+
+  if (input) {
+    if (esc) {
+      // restore state
+      document.execCommand('undo');
+      el.blur();
+    } else if (nl) {
+      // save
+      data[el.getAttribute('data-name')] = el.innerHTML;
+
+      // we could send an ajax request to update the field
+      /*
+      $.ajax({
+        url: window.location.toString(),
+        data: data,
+        type: 'post'
+      });
+      */
+      log(JSON.stringify(data));
+
+      el.blur();
+      event.preventDefault();
+    }
+  }
+}, true);
+
+function log(s) {
+  document.getElementById('debug').innerHTML = 'value changed to: ' + s;
+}
