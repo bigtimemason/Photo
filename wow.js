400820663b0f4a9b55c02f986bb1adbfa3cdeae2
Mason Miller <44513924+bigtimemason@users.noreply.github.com>
2019-05-11 15:57:05 -0400

    Update wow.js

--- wow.js
+++ wow.js
@@ -1,21 +1,35 @@
+document.addEventListener('keydown', function (event) {
+  var esc = event.which == 27,
+      nl = event.which == 13,
+      el = event.target,
+      input = el.nodeName != 'INPUT' && el.nodeName != 'TEXTAREA',
+      data = {};
 
+  if (input) {
+    if (esc) {
+      // restore state
+      document.execCommand('undo');
+      el.blur();
+    } else if (nl) {
+      // save
+      data[el.getAttribute('data-name')] = el.innerHTML;
 
-// Load content onload if it exists in localStorage
-window.onload = function() {
-	if(localStorage.getItem('content')) {
-		document.querySelector('.content').innerHTML = localStorage.getItem('content');
+      // we could send an ajax request to update the field
+      /*
+      $.ajax({
+        url: window.location.toString(),
+        data: data,
+        type: 'post'
+      });
+      */
+      log(JSON.stringify(data));
+
+      el.blur();
+      event.preventDefault();
+    }
   }
+}, true);
+
+function log(s) {
+  document.getElementById('debug').innerHTML = 'value changed to: ' + s;
 }
-
-let editBtn = document.querySelector('#edit_content');
-let content = document.querySelector('.content');
-
-editBtn.addEventListener('click', () => {
-  // Toggle contentEditable on button click
-	content.contentEditable = !content.isContentEditable;
-  
-  // If disabled, save text
-  if(content.contentEditable === 'false') {
-  	localStorage.setItem('content', content.innerHTML);
-  }
-});
