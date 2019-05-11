34b7a0fbdbe809e76d690df399320da8f4c5d705
Mason Miller <44513924+bigtimemason@users.noreply.github.com>
2019-05-10 21:27:44 -0400

    Update wow.js

--- wow.js
+++ wow.js
@@ -1,14 +1,4 @@
-Here's a way you can do this using vanilla JS.
 
-Here's JSFiddle since StackOverflow doesn't allow localStorage to be executed on the website as a security feature.
-
-How it works:
-
-Using window.onload = function() {...}, check if 'content' key exists in localStorage
-If it does, load the data into <div class="content">
-Upon pressing Edit button, contentEditable is toggled
-Here you can use any method to save the data. I used content.contentEditable === 'false' to save the innerHTML data to 'content' key.
-To Note: localStorage is saved in your browser locally, use databases or anything similar to display edits to all viewers.
 
 // Load content onload if it exists in localStorage
 window.onload = function() {
